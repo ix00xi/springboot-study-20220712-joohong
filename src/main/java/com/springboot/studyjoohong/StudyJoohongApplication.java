d205a0e05fc28cc14a4e12781fd01078adc280e2
ix00xi <izx43@naver.com>
2022-07-12 23:04:19 +0900

    20220712 springboot 기본세팅 및 첫 페이지 열어보기

---
+++ src/main/java/com/springboot/studyjoohong/StudyJoohongApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.studyjoohong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJoohongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJoohongApplication.class, args);
+	}
+	
+	
+}
---
+++ src/main/java/com/springboot/studyjoohong/web/controller/MethodStudyController.java
@@ -0,0 +1,17 @@
+package com.springboot.studyjoohong.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MethodStudyController {
+	
+	@GetMapping({"/", "/index"})
+	public String getMethod() {
+		return "method/get";
+	}
+	@GetMapping("/test")
+	public String getMethod2() {
+		return"method/test";
+	}
+}
